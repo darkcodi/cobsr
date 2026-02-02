bf24c6395611d59f68fe433739699c921ea1fa9b
darkcodi <trooper982@gmail.com>
2026-02-02 07:30:14 -0600

    Improve test experience

--- src/lib.rs
+++ src/lib.rs
@@ -69,175 +69,63 @@ pub fn cobs_encode_in_place(buf: &mut [u8], len: usize) -> Result<usize, Error> 
 mod tests {
     use super::*;
 
-    #[test]
-    fn encode_wiki1() {
-        let mut buf = [0x00, 0x00, 0x00];
-        let len = 1;
+    fn generate_buffer<const N: usize>(s: &str) -> ([u8; N], usize) {
+        let mut buf = [0; N];
+        let mut iter = s.split_whitespace();
+        let mut index = 0;
+        let mut previous_number = 0;
+        let mut should_generate_sequence = false;
+
+        while let Some(w) = iter.next() {
+            if w == ".." || w == "..." {
+                should_generate_sequence = true;
+            } else {
+                let n: u8 = u8::from_str_radix(w, 16).unwrap();
+                if should_generate_sequence {
+                    for i in (previous_number + 1)..=n {
+                        buf[index] = i;
+                        index += 1;
+                    }
+                    previous_number = n;
+                    should_generate_sequence = false;
+                } else {
+                    buf[index] = n;
+                    previous_number = n;
+                    index += 1;
+                }
+            }
+        }
+
+        (buf, index)
+    }
+
+    fn encode_test<const N: usize>(before: &str, after: &str) {
+        let (mut buf, len) = generate_buffer::<N>(before);
+        let (expected_buf, expected_len) = generate_buffer::<N>(after);
         let encode_result = cobs_encode_in_place(&mut buf, len);
         assert!(encode_result.is_ok());
         let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 2);
-        assert_eq!(buf[..new_len], [0x01, 0x01]);
+        assert_eq!(new_len, expected_len);
+        assert_eq!(buf[..new_len], expected_buf[..expected_len]);
     }
 
     #[test]
-    fn encode_wiki2() {
-        let mut buf = [0x00, 0x00, 0x00, 0x00];
-        let len = 2;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 3);
-        assert_eq!(buf[..new_len], [0x01, 0x01, 0x01]);
-    }
-
-    #[test]
-    fn encode_wiki3() {
-        let mut buf = [0x00, 0x11, 0x00, 0x00, 0x00];
-        let len = 3;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 4);
-        assert_eq!(buf[..new_len], [0x01, 0x02, 0x11, 0x01]);
-    }
-
-    #[test]
-    fn encode_wiki4() {
-        let mut buf = [0x11, 0x22, 0x00, 0x33, 0x00, 0x00];
-        let len = 4;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 5);
-        assert_eq!(buf[..new_len], [0x03, 0x11, 0x22, 0x02, 0x33]);
-    }
-
-    #[test]
-    fn encode_wiki5() {
-        let mut buf = [0x11, 0x22, 0x33, 0x44, 0x00, 0x00];
-        let len = 4;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 5);
-        assert_eq!(buf[..new_len], [0x05, 0x11, 0x22, 0x33, 0x44]);
-    }
-
-    #[test]
-    fn encode_wiki6() {
-        let mut buf = [0x11, 0x00, 0x00, 0x00, 0x00, 0x00];
-        let len = 4;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 5);
-        assert_eq!(buf[..new_len], [0x02, 0x11, 0x01, 0x01, 0x01]);
-    }
-
-    #[test]
-    fn encode_wiki7() {
-        let mut buf = [0; 300];
-        for i in 0x01..=0xFE {
-            buf[i - 1] = i as u8;
-        }
-        let len = 254;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 255);
-        assert_eq!(buf[0], 0xFF);
-        for i in 0x01..=0xFE {
-            assert_eq!(buf[i], i as u8);
-        }
-    }
-
-    #[test]
-    fn encode_wiki8() {
-        let mut buf = [0; 300];
-        for i in 0x00..=0xFE {
-            buf[i] = i as u8;
-        }
-        let len = 255;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 256);
-        assert_eq!(buf[0], 0x01);
-        assert_eq!(buf[1], 0xFF);
-        for i in 0x01..=0xFE {
-            assert_eq!(buf[i + 1], i as u8);
-        }
-    }
-
-    #[test]
-    fn encode_wiki9() {
-        let mut buf = [0; 300];
-        for i in 0x01..=0xFF {
-            buf[i - 1] = i as u8;
-        }
-        let len = 255;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 257);
-        assert_eq!(buf[0], 0xFF);
-        for i in 0x01..=0xFE {
-            assert_eq!(buf[i], i as u8);
-        }
-        assert_eq!(buf[255], 0x02);
-        assert_eq!(buf[256], 0xFF);
-    }
-
-    #[test]
-    fn encode_wiki10() {
-        let mut buf = [0; 300];
-        for i in 0x02..=0xFF {
-            buf[i - 2] = i as u8;
-        }
-        buf[254] = 0x00;
-        let len = 255;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 257);
-        assert_eq!(buf[0], 0xFF);
-        for i in 0x02..=0xFF {
-            assert_eq!(buf[i - 1], i as u8);
-        }
-        assert_eq!(buf[255], 0x01);
-        assert_eq!(buf[256], 0x01);
-    }
-
-    #[test]
-    fn encode_wiki11() {
-        let mut buf = [0; 300];
-        for i in 0x03..=0xFF {
-            buf[i - 3] = i as u8;
-        }
-        buf[253] = 0x00;
-        buf[254] = 0x01;
-        let len = 255;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 256);
-        assert_eq!(buf[0], 0xFE);
-        for i in 0x03..=0xFF {
-            assert_eq!(buf[i - 2], i as u8);
-        }
-        assert_eq!(buf[254], 0x02);
-        assert_eq!(buf[255], 0x01);
+    fn encode_wiki_tests() {
+        encode_test::<3>("00", "01 01");
+        encode_test::<4>("00 00", "01 01 01");
+        encode_test::<5>("00 11 00", "01 02 11 01");
+        encode_test::<6>("11 22 00 33", "03 11 22 02 33");
+        encode_test::<6>("11 22 33 44", "05 11 22 33 44");
+        encode_test::<6>("11 00 00 00", "02 11 01 01 01");
+        encode_test::<300>("01 02 03 ... FD FE", "FF 01 02 03 ... FD FE");
+        encode_test::<300>("00 01 02 ... FC FD FE", "01 FF 01 02 ... FC FD FE");
+        encode_test::<300>("01 02 03 ... FD FE FF", "FF 01 02 03 ... FD FE 02 FF");
+        encode_test::<300>("02 03 04 ... FE FF 00", "FF 02 03 04 ... FE FF 01 01");
+        encode_test::<300>("03 04 05 ... FF 00 01", "FE 03 04 05 ... FF 02 01");
     }
 
     #[test]
     fn encode_example_a() {
-        let mut buf = [0x2F, 0xA2, 0x00, 0x92, 0x73, 0x02, 0x00, 0x00, 0x00];
-        let len = 6;
-        let encode_result = cobs_encode_in_place(&mut buf, len);
-        assert!(encode_result.is_ok());
-        let new_len = encode_result.unwrap();
-        assert_eq!(new_len, 7);
-        assert_eq!(buf[..new_len], [0x03, 0x2F, 0xA2, 0x04, 0x92, 0x73, 0x02]);
+        encode_test::<10>("2F A2 00 92 73 02", "03 2F A2 04 92 73 02");
     }
 }
